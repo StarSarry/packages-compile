04bdb839e0c5ce42a42f92e29690bfb13e36eb6f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-12-18 22:38:40 +0800

    Update custom2.sh

--- devices/common/custom2.sh
+++ devices/common/custom2.sh
@@ -10,7 +10,7 @@ sed -i -E "s#git\.openwrt\.org/(openwrt|feed|project)#github.com/openwrt#" feeds
 ./scripts/feeds install -a
 
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
-rm -rf package/feeds/packages/{netdata,cloudreve,smartdns,vsftpd,p910nd,aria2,ariang,coremark,watchcat,dockerd,tessdata,telegraf,oci-runtime-tools,micropython,frp}
+rm -rf package/feeds/packages/{netdata,cloudreve,smartdns,vsftpd,p910nd,aria2,ariang,coremark,watchcat,dockerd,frp}
 
 cp -f devices/common/.config .config
 

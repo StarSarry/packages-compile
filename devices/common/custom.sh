e3280d36953cf1ff4f00cf8a981f207aa07d0b80
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-12-22 22:08:32 +0800

    Update custom.sh

--- devices/common/custom.sh
+++ devices/common/custom.sh
@@ -33,7 +33,7 @@ rm -Rf feeds/base_root/package/firmware
 rm -Rf feeds/base_root/package/network/!(services|utils)
 rm -Rf feeds/base_root/package/network/services/!(ppp)
 rm -Rf feeds/base_root/package/system/!(opkg|ubus|uci|ca-certificates)
-rm -Rf feeds/base_root/package/kernel/!(cryptodev-linux)
+rm -Rf feeds/base_root/package/kernel/!(cryptodev-linux||bpf-headers)
 #COMMENT
 
 status=$(curl -H "Authorization: token $REPO_TOKEN" -s "https://api.github.com/repos/kiddin9/kwrt-packages/actions/runs" | jq -r '.workflow_runs[0].status')
